d6bfaf255ebf1b1d44b8eb1ed3be225a303c1fea
Ze1deR1 <vladimir_pnz@bk.ru>
2026-06-25 14:41:10 +0300

    Добавил проверку правильности ручного ввода и динамическое выделение памяти

--- practice.cpp
+++ practice.cpp
@@ -1,17 +1,18 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
 void insertionSort(int arr[], int n) {
     int i, key, j;
 
     for (i = 1; i < n; i++) {
-        key = arr[i];
+        key = arr[i];    
         j = i - 1;
 
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
         }
-        arr[j + 1] = key;
+        arr[j + 1] = key;  
     }
 }
 
@@ -23,11 +24,21 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    int arr[100];  // фиксированный массив до 100 элементов
     int n;
 
     printf("Введите количество элементов массива: ");
     scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Ошибка: количество элементов должно быть положительным!\n");
+        return 1;
+    }
+
+    int* arr = (int*)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Ошибка выделения памяти!\n");
+        return 1;
+    }
 
     printf("Введите %d целых чисел:\n", n);
     for (int i = 0; i < n; i++) {
@@ -42,6 +53,8 @@ int main() {
 
     printf("Отсортированный массив: ");
     printArray(arr, n);
+
+    free(arr);
 
     return 0;
 }
